96db7cb9995ef5967e287155702a13b3e01cce24
15138730370 <1037483576@qq.com>
2018-01-04 21:13:19 +0800

    save to back

--- README.md
+++ README.md
@@ -1,5 +1,14 @@
 ## debuger for angular mobile
 
 ```
-默认。累计点击 每6次显示调试
+默认。累计点击 每3次显示调试
+```
+
+
+```html
+<div class="main" debuger>
+    <div class="empty">
+        长按3秒触发调试
+    </div>
+</div>
 ```
--- src/app/debuger.ts
+++ src/app/debuger.ts
@@ -1,24 +1,51 @@
 import { Directive, HostListener, Input } from '@angular/core';
 import { LoaderService } from 'meepo-loader';
+import { IntervalService } from 'meepo-event';
+
 declare const VConsole: any;
-@Directive({ selector: '[debuger]' })
+@Directive({
+    selector: '[debuger]',
+    providers: [
+        IntervalService
+    ]
+})
 export class DebugerDirective {
-    @Input() debuger: number = 6;
+    @Input() debuger: number = 3;
     num: number = 0;
     loaded: boolean = false;
     _debuger: any;
-    @HostListener('click', ['$event'])
-    onTouch() {
-        this.num++;
-        if (this.num % 6 == 0) {
-            this.showDebuger();
-        } else {
-            this.hideDebuger();
-        }
+    @HostListener('touchstart', ['$event'])
+    onTouchStart() {
+        // this.showDebuger();
+        this.interval.clearAll();
+        this.hideDebuger();
+        this.interval.add(() => {
+            this.num++;
+            if (this.num % 6 == 0) {
+                this.showDebuger();
+            } else {
+                this.hideDebuger();
+            }
+            console.log(this.num);
+        }, 1000);
     }
+
+    @HostListener('touchcancel', ['$event'])
+    onTouchCancel() {
+        this.interval.clearAll();
+    }
+
+    @HostListener('touchend', ['$event'])
+    onTouchEnd() {
+        this.interval.clearAll();
+    }
+
     constructor(
-        public loader: LoaderService
-    ) { }
+        public loader: LoaderService,
+        public interval: IntervalService
+    ) {
+
+    }
 
     showDebuger() {
         if (this._debuger) {
@@ -31,6 +58,7 @@ export class DebugerDirective {
             });
         }
         this.loaded = true;
+        this.interval.clearAll();
     }
 
     hideDebuger() {
